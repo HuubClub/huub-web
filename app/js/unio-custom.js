78282a11b0221ea8721f24e94470067063eb7bb0
Takayuki Tominaga <touka_tt@jhotmail.co.jp>
2020-05-19 01:13:57 +0900

    update lineup

--- app/js/unio-custom.js
+++ app/js/unio-custom.js
@@ -171,8 +171,8 @@ $(function() {
   // --------------------------------------------- //
   // KBW-Countdown Start
   // --------------------------------------------- //
-  $('#countdown').countdown({until: $.countdown.UTCDate(+9, 2020, 6-1, 16, 22, 0, 0, 0), format: 'D'});
-  $('#countdown-large').countdown({until: $.countdown.UTCDate(+9, 2020, 6-1, 16, 22, 0, 0, 0), format: 'DHMS'});
+  $('#countdown').countdown({until: $.countdown.UTCDate(+540, 2020, 6-1, 20, 19, 0, 0, 0), format: 'D'});
+  $('#countdown-large').countdown({until: $.countdown.UTCDate(+540, 2020, 6-1, 20, 19, 0, 0, 0), format: 'DHMS'});
   // --------------------------------------------- //
   // KBW-Countdown End
   // --------------------------------------------- //
